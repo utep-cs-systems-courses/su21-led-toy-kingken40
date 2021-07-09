a44f76b9270de9df6d9b69c706fff900f80432b9
kingken40 <45611070+kingken40@users.noreply.github.com>
2021-07-08 19:38:29 -0600

    Update wdInterruptHandler.c

--- demos/blink_demo/wdInterruptHandler.c
+++ demos/blink_demo/wdInterruptHandler.c
@@ -8,7 +8,8 @@ __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
     red_toggle_on(); /* turn both on to have both flash every second */
     green_toggle_on();
     blink_count = 0;
-    red_toggle_off();
+    // to make lights dim
+    red_toggle_off();   
     green_toggle_off();
   }
   else if(blink_count <= 250){

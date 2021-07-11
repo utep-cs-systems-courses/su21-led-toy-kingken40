0bcd9166bfb43d94d36e40daf12dbe1de46f886f
kingken40 <45611070+kingken40@users.noreply.github.com>
2021-07-11 15:48:17 -0600

    Updated blinking Led
    off > dim > bright

--- demos/blink_demo/wdInterruptHandler.c
+++ demos/blink_demo/wdInterruptHandler.c
@@ -9,11 +9,16 @@ __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
     red_toggle_on(); /* turn both on to have both flash every second */
     green_toggle_on();
     blink_count = 0;
-    // to make lights dim
-    red_toggle_off();   
-    green_toggle_off();
   }
-  else if(blink_count <= 250){
+  else if(blink_count > 125 && blink_count < 250){
+    // to make lights dim
+    red_toggle_on(); 
+    green_toggle_on();
+    red_toggle_off();
+    green_toggle_off();
+    blink_count++;
+  }
+  else if(blink_count <= 125){
     red_toggle_off();
     green_toggle_off();
     blink_count++;

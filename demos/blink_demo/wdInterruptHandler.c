5e8e752ec01bb1730c98fc4345c1e455959f7c4d
kingken40 <offdawall316@gmail.com>
2021-07-11 15:44:39 -0600

    updated blinking led

--- demos/blink_demo/wdInterruptHandler.c
+++ demos/blink_demo/wdInterruptHandler.c
@@ -5,16 +5,22 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (blink_count == 250) {
+   if (blink_count == 250) {
      red_toggle_on(); /*turn both on to have both flash every second */
      green_toggle_on(); 
      blink_count = 0;
-     red_toggle_off();
-     green_toggle_off();
    }
-  else if(blink_count <= 250){
+  else if(blink_count > 125 && blink_count < 250){
+    red_toggle_on();
+    green_toggle_on();
     red_toggle_off();
     green_toggle_off();
     blink_count++;
   }
+  else if(blink_count <= 125){
+    red_toggle_off();
+    green_toggle_off();
+    blink_count++;
+  }
+   
 }
